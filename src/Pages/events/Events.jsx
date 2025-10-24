d0cabbf4bb4718f2c70b9d0964512ccaee1c0915
aromatic32 <abdelrahman.wael1233@gmail.com>
2025-10-24 10:43:32 +0300

    updated home page to use same eventCard

--- src/Pages/events/Events.jsx
+++ src/Pages/events/Events.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import EventCard from "./componant/EventCard";
+import EventCard from "../../Components/eventCard/EventCard";
+
 const Events = ({ setSelectedEvent }) => {
   const [event] = useState([
     {
