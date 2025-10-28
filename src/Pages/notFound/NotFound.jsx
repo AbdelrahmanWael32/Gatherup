8e15aed7afce37b458458fbe4dc8fa91cb5a26c5
aromatic32 <abdelrahman.wael1233@gmail.com>
2025-10-28 19:08:02 +0300

    updated NotFound Page

--- src/Pages/notFound/NotFound.jsx
+++ src/Pages/notFound/NotFound.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { Button } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="flex justify-center flex-col items-center gap-8">
       <h1 className="text-5xl">Page Not Found</h1>
+      <div className="flex justify-between w-1/4">
+        <Link to={-1}>
+          <Button className="bg-brand-dark">Go Back</Button>
+        </Link>
+        <Link to="/">
+          <Button className="bg-brand-dark">Go to Home</Button>
+        </Link>
+      </div>
     </div>
   );
 };
