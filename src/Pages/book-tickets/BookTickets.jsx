3d971cf9329458a62fab2459580196a565b0fd78
unknown <janasobih1@gmail.com>
2025-10-30 15:49:59 +0300

    showTickets dashboard

--- src/Pages/book-tickets/BookTickets.jsx
+++ src/Pages/book-tickets/BookTickets.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input, Typography, Button } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
+
 const BookTickets = () => {
-  const [cvv, setCvv] = React.useState("");
-  const [cardNumber, setCardNumber] = React.useState("");
-  const [expirationDate, setExpirationDate] = React.useState("");
-  const [cardholderName, setCardholderName] = React.useState("");
   const navigate = useNavigate();
+
+  const [card, setCard] = useState({
+    cardholderName: "",
+    cardNumber: "",
+    cvv: "",
+    expirationDate: "",
+  });
+
+  const [errors, setErrors] = useState([]);
+
+  const handelCheckOut = (e) => {
+    e.preventDefault();
+    const newErrors = [];
+    const { cvv, cardholderName, cardNumber, expirationDate } = card;
+    if (
+      cvv === "" ||
+      cardholderName === "" ||
+      cardNumber === "" ||
+      expirationDate === ""
+    ) {
+      newErrors.push("Please fill in all required fields");
+    }
+    setErrors(newErrors);
+    if (newErrors.length === 0) {
+      console.log(" Payment details are valid!");
+    } else {
+      console.warn(" Validation failed:", newErrors);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-auto md:h-[100vh]">
-      <div className="w-full max-w-sm ">
+      <form onSubmit={handelCheckOut} className="w-full max-w-sm p-10   rounded-lg shadow-md ">
+        <h2 className="text-xl font-semibold text-gray-800 mb-4">Payment Details</h2>
         <Typography
           variant="small"
           color="blue-gray"
@@ -19,15 +47,15 @@ const BookTickets = () => {
         </Typography>
         <Input
           placeholder="e.g John Doe"
-          className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 focus:placeholder:text-brand-primary  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none "
           labelProps={{
             className: "before:content-none after:content-none",
           }}
           containerProps={{
             className: "min-w-0",
           }}
-          value={cardholderName}
-          onChange={(e) => setCardholderName(e.target.value)}
+          value={card.cardholderName}
+          onChange={(e) => setCard({ ...card, cardholderName: e.target.value })}
         />
 
         <Typography
@@ -40,18 +68,18 @@ const BookTickets = () => {
         <Input
           placeholder="1234 5678 9012 3456"
           maxLength={19}
-          className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 focus:placeholder:text-brand-primary  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
           labelProps={{
             className: "before:content-none after:content-none",
           }}
           containerProps={{
             className: "min-w-0",
           }}
-          value={cardNumber
+          value={card.cardNumber
             .replace(/\s/g, "")
             .replace(/(\d{4})/g, "$1 ")
             .trim()}
-          onChange={(e) => setCardNumber(e.target.value)}
+          onChange={(e) => setCard({ ...card, cardNumber: e.target.value })}
         />
 
         <div className="mt-4 flex">
@@ -67,18 +95,20 @@ const BookTickets = () => {
               placeholder="MM/YY"
               maxLength={5}
               pattern="\d{2}/\d{2}"
-              className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 focus:placeholder:text-brand-primary  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               containerProps={{
                 className: "!min-w-0",
               }}
-              value={expirationDate
+              value={card.expirationDate
                 .replace(/[^0-9]/g, "")
                 .replace(/(\d{2})(\d{1,2})/, "$1/$2")
                 .substring(0, 5)}
-              onChange={(e) => setExpirationDate(e.target.value)}
+              onChange={(e) =>
+                setCard({ ...card, expirationDate: e.target.value })
+              }
             />
           </div>
           <div className="w-full md:w-4/12">
@@ -93,15 +123,15 @@ const BookTickets = () => {
               placeholder="123"
               maxLength={3}
               pattern="\d{3}"
-              className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 focus:placeholder:text-brand-primary  placeholder:opacity-100 focus:!border-brand-primary [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               containerProps={{
                 className: "!min-w-0",
               }}
-              value={cvv.replace(/[^0-9]/g, "").replace(/(\..*)\./g, "$1")}
-              onChange={(e) => setCvv(e.target.value)}
+              value={card.cvv.replace(/[^0-9]/g, "").replace(/(\..*)\./g, "$1")}
+              onChange={(e) => setCard({ ...card, cvv: e.target.value })}
             />
           </div>
         </div>
@@ -113,9 +143,18 @@ const BookTickets = () => {
           >
             Cancel
           </Button>
-          <Button className="mt-4 bg-brand-primary">Done</Button>
+          <Button onClick={handelCheckOut} className="mt-4 bg-brand-primary">
+            Done
+          </Button>
         </div>
-      </div>
+        {errors.length > 0 && (
+          <div className="mt-4 text-center text-red-500 text-sm">
+            {errors.map((err, i) => (
+              <p key={i}> {err}</p>
+            ))}
+          </div>
+        )}
+      </form>
     </div>
   );
 };
