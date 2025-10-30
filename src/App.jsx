b4063c4227e2c9a83a21dc042d147d91016af8f0
aromatic32 <abdelrahman.wael1233@gmail.com>
2025-10-30 14:09:22 +0300

    temp session storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
 import UserLayout from "./UserLayout";
 import AdminLayout from "./AdminLayout";
 import AuthContext from "./hooks/useLogin";
--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -3,12 +3,26 @@ import { createContext, useContext, useState } from "react";
 const GetAuth = createContext();
 
 const AuthContext = ({ children }) => {
-  const [userStatus, setUserStatus] = useState(false);
-  const [userInfo, setUserInfo] = useState(null);
+  const [userStatus, setUserStatus] = useState(
+    sessionStorage.hul ? true : false
+  );
+  const [userInfo, setUserInfo] = useState(
+    sessionStorage.lui ? sessionStorage.lui : null
+  );
+
+  const updateUserInfo = (updatedInfo) => {
+    sessionStorage.lui = updatedInfo;
+    setUserInfo(updatedInfo);
+  };
+
+  const updateUserStatus = (updatedInfo) => {
+    sessionStorage.hul = updatedInfo;
+    setUserStatus(updatedInfo);
+  };
 
   return (
     <GetAuth.Provider
-      value={{ userStatus, userInfo, setUserInfo, setUserStatus }}
+      value={{ userStatus, userInfo, updateUserInfo, updateUserStatus }}
     >
       {children}
     </GetAuth.Provider>
