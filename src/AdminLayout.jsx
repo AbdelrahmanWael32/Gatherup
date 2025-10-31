d026c6f81e9ea400ae262f7fb67620c9f5cb8db7
unknown <janasobih1@gmail.com>
2025-10-31 22:03:19 +0200

    ticket categories in dashboard

--- src/AdminLayout.jsx
+++ src/AdminLayout.jsx
@@ -11,25 +11,56 @@ const AdminLayout = () => {
       id: 1,
       image: "/sport1.png",
       title: "Youth Basketball Championship",
-      price: 100,
       date: "December 15, 2025",
       location: "Al Gezira Sports Club – Cairo",
+      ticketCategories: [
+        {
+          type: "Standard",
+          price: 100,
+        },
+        {
+          type: "Premium",
+          price: 180,
+        },
+        {
+          type: "VIP",
+          price: 300,
+        },
+      ],
     },
     {
       id: 2,
       image: "/event2.png",
       title: "Outdoor Movie Night",
-      price: 75,
       date: "October 3, 2025",
       location: "Zed Park – Sheikh Zayed, Giza",
+      ticketCategories: [
+        {
+          type: "Standard",
+          price: 75,
+        },
+      ],
     },
     {
       id: 3,
       image: "/event4.png",
       title: "Summer Beats Live Concert",
-      price: 250,
       date: "August 8, 2025",
       location: "Cairo Festival City Amphitheater – New Cairo",
+      ticketCategories: [
+        {
+          type: "Standard",
+          price: 250,
+        },
+        {
+          type: "Premium",
+          price: 400,
+        },
+        {
+          type: "VIP",
+          price: 600,
+        },
+      ],
     },
   ]);
   return (
@@ -39,7 +70,7 @@ const AdminLayout = () => {
         <Route index element={<AdminDashboard event={event} />} />
         <Route path="tickets" element={<ShowTickets event={event} />} />
         <Route path="ticket-details/:id" element={<TicketDetails />} />
-        <Route path="edit" element={<EditTickets/>}/>
+        <Route path="edit" element={<EditTickets />} />
       </Routes>
     </div>
   );
