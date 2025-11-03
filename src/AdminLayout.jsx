96ef7ee858f8ccef9250b43832ac761acd1a2e8f
maryammohamed122 <maryammohamedhussian@gmail.com>
2025-11-03 10:11:54 +0200

    add ticket update

--- src/AdminLayout.jsx
+++ src/AdminLayout.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "./Pages/admin-pages/AdminDashboard";
 import ShowTickets from "./Pages/admin-pages/showTickets/ShowTickets";
-import AdminHeader from "./Pages/admin-pages/components/AdminHeader";
+import AdminHeader from "./Pages/admin-pages/Components/AdminHeader";
 import { useState } from "react";
 import AddTicket from "./Pages/admin-pages/addTicket/AddTicket";
 
