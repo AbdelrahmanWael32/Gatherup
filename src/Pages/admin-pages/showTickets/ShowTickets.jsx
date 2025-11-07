16e79aba2e62564fb500482e4cb6807c2b7c044d
unknown <janasobih1@gmail.com>
2025-11-07 11:03:35 +0200

    edit design show tickets

--- src/Pages/admin-pages/showTickets/ShowTickets.jsx
+++ src/Pages/admin-pages/showTickets/ShowTickets.jsx
@@ -12,7 +12,7 @@ const ShowTickets = ({ event }) => {
 
   return (
     <div className="min-h-screen">
-      <div className="md:mx-20 mt-10 rounded-xl shadow-md border border-gray-100 p-6">
+      <div className="md:mx-5 xl:mx-20 mt-10 rounded-xl shadow-md border border-gray-100 p-6">
         <div className="flex justify-between items-center mb-6">
           <h2 className="sm:text-2xl font-semibold text-brand-primary">
             Tickets Management
@@ -25,36 +25,32 @@ const ShowTickets = ({ event }) => {
           </button>
         </div>
 
-        <div>
-          <div className="hidden lg:grid grid-cols-6 bg-blue-100 py-3 px-2 rounded-t-lg text-brand-dark font-medium text-center">
-            <div className="col-span-1">Event</div>
-            <div className="col-span-1">Date</div>
-            <div className="col-span-1">Location (EGP)</div>
-            <div className="col-span-1">Category</div>
-            <div className="col-span-1">Price</div>
-            <div className="col-span-1">Actions</div>
-          </div>
-
-          {event && event.length > 0 ? (
-            event.map((ticket) => (
-              <div
-                key={ticket.id}
-                className="lg:grid grid-cols-6 items-center border-b border-gray-200 py-3 px-4 hover:bg-gray-50 transition text-center"
-              >
-                <div className="flex items-center gap-3 justify-center">
+        {event && event.length > 0 ? (
+          event.map((ticket) => (
+            <div
+              key={ticket.id}
+              className="flex flex-col md:flex-row gap-5 items-center justify-between border-b border-gray-200 py-3  hover:bg-gray-50 transition text-center"
+            >
+              <div className="flex gap-3 justify-center items-center">
+                <div>
                   <img
                     src={ticket.image}
                     className="w-12 h-12 rounded-lg object-cover"
                   />
-                  <div className="font-medium text-brand-secondary">
+                </div>
+                <div>
+                  <div className="font-bold text-brand-secondary">
                     {ticket.title}
                   </div>
+                  <div className="text-brand-secondary my-4">{ticket.date}</div>
+                  <div className="text-brand-secondary my-4">
+                    {ticket.location}
+                  </div>
                 </div>
+              </div>
 
-                <div className="text-brand-secondary my-4">{ticket.date}</div>
-                <div className="text-brand-secondary my-4">{ticket.location}</div>
-
-                <div className="text-brand-secondary hidden lg:block">
+              <div className="flex gap-[5rem]">
+                <div className="text-brand-secondary ">
                   {ticket.ticketCategories.map((Categories, index) => (
                     <div
                       key={index}
@@ -64,7 +60,7 @@ const ShowTickets = ({ event }) => {
                     </div>
                   ))}
                 </div>
-                <div className="text-brand-secondary hidden lg:block">
+                <div className="text-brand-secondary">
                   {ticket.ticketCategories.map((Categories, index) => (
                     <div
                       key={index}
@@ -74,57 +70,33 @@ const ShowTickets = ({ event }) => {
                     </div>
                   ))}
                 </div>
-
-                {/* mobile */}
-                <div className="lg:hidden flex justify-between items-center gap-4 mb-4">
-                  <div className="text-brand-secondary items-center flex-1">
-                    {ticket.ticketCategories.map((Categories, index) => (
-                      <div
-                        key={index}
-                        className="border-b border-gray-100 last:border-none py-1"
-                      >
-                        {Categories.type}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="text-brand-secondary flex-1">
-                    {ticket.ticketCategories.map((Categories, index) => (
-                      <div
-                        key={index}
-                        className="border-b border-gray-100 last:border-none py-1"
-                      >
-                        {Categories.price}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                
-                <div className="flex flex-wrap xl:flex-nowrap justify-center gap-2">
-                  <Link
-                    to={`/admin/ticket-details/${ticket.id}`}
-                    onClick={() => setSelectedTicket(ticket)}
-                    className="bg-brand-primary text-white px-3 py-1 rounded hover:bg-blue-600"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    to="/admin/edit"
-                    className="bg-brand-primary text-white px-3 py-1 rounded hover:bg-blue-600"
-                  >
-                    Edit
-                  </Link>
-                  <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
-                    Delete
-                  </button>
-                </div>
               </div>
-            ))
-          ) : (
-            <p className="text-center py-6 text-brand-secondary italic">
-              No tickets found.
-            </p>
-          )}
-        </div>
+
+              <div className="flex flex-wrap xl:flex-nowrap justify-center gap-2">
+                <Link
+                  to={`/admin/ticket-details/${ticket.id}`}
+                  onClick={() => setSelectedTicket(ticket)}
+                  className="bg-brand-primary text-white px-3 py-1 rounded hover:bg-blue-600"
+                >
+                  View
+                </Link>
+                <Link
+                  to="/admin/edit"
+                  className="bg-brand-primary text-white px-3 py-1 rounded hover:bg-blue-600"
+                >
+                  Edit
+                </Link>
+                <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center py-6 text-brand-secondary italic">
+            No tickets found.
+          </p>
+        )}
       </div>
     </div>
   );
